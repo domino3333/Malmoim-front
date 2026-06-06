31e32ca1b7f1953ca4add7491e44a0b740f805ad
domino3333 <busster@naver.com>
2026-06-06 12:55:21 +0900

    액세스 토큰을 session storage에 저장 브라우저 탭을 닫으면 사라지고, localstorage보다 유지시간이 짧지만 XSS에 취약

--- src/api/auth/authApi.jsx
+++ src/api/auth/authApi.jsx
@@ -6,10 +6,14 @@ const prefix = "/api/auth"
 
 
 export const login = (data)=>{
-    const result = axios.post(`${ApiHost}${prefix}/login`,data,null)
+    const result = axios.post(`${ApiHost}${prefix}/login`,data,null);
+
+    //로그인 시 액세스토큰 저장
+    sessionStorage.setItem('accessToken',result.data);
 }
 
 
 export const signUp = (data)=>{
-    const result = axios.post(`${ApiHost}${prefix}/signUp`,data,null)
+    const result = axios.post(`${ApiHost}${prefix}/signUp`,data,null);
+    
 }
