447476f6819019706d3370ab4cf5b73441fbccb2
domino3333 <busster@naver.com>
2026-06-09 08:46:44 +0900

    공개여부에 따라 비밀번호 창 띄우기

--- src/components/modal/CreateModal.jsx
+++ src/components/modal/CreateModal.jsx
@@ -1,18 +1,38 @@
 import { Modal } from "react-bootstrap";
 import "../../css/modal/CreateModal.css"
+import { useState } from "react";
 
 
 
-const CreateModal = ({ show, setShow }) => {
+const CreateModal = ({ show, setShow, title }) => {
+
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const observeCheckBox = (e) => {
+        setIsChecked(e.target.checked);
+    }
 
 
     return (<>
         <Modal show={show} onHide={() => setShow(false)}>
             <div className="modal-body">
+                <h4>{title}</h4>
                 <input className="modal-title" type="text" placeholder="제목" />
-                <input className="modal-password" type="password" placeholder="비밀번호" />
-                
+                <input className="modal-capacity" type="number" placeholder="정원" />
 
+                <label htmlFor="checkBoxTitle">
+                    비공개
+                    <input className="checkBoxTitle" type="checkbox" onChange={observeCheckBox} />
+                </label>
+
+                {isChecked ?
+                    <input className="modal-password" type="password" placeholder="비밀번호" />
+                    : null}
+
+                <button>
+                    만들기
+                </button>
             </div>
 
         </Modal>
--- src/pages/dashboard/CreateDetailPage.jsx
+++ src/pages/dashboard/CreateDetailPage.jsx
@@ -38,7 +38,7 @@ const CreateDetailPage = () => {
             </div>
         </div>
 
-        <CreateModal show={show} setShow={setShow}/>
+        <CreateModal show={show} setShow={setShow} title={"청중QnA"}/>
 
         
 
