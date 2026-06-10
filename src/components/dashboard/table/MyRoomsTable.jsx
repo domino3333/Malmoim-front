05fb5bbedb1aa7fe8e417a75a454edc103852578
domino3333 <busster@naver.com>
2026-06-10 14:43:16 +0900

    테이블 css 수정

--- src/components/dashboard/table/MyRoomsTable.jsx
+++ src/components/dashboard/table/MyRoomsTable.jsx
@@ -1,23 +1,44 @@
 
+import "../../../css/dashboard/MyRoomsTable.css"
 
 
 
-const MyRoomsTable = ({rooms}) => {
+const MyRoomsTable = ({ rooms }) => {
 
     return (<>
 
-        <table>
-            {rooms.map((room) =>
-                <tr key={room.no}>
-                    <td>
-                        {room.title}
-                    </td>
-                    <td>
-                        {room.hostNo}
-                    </td>
+        <table className="div-myRoomsTable">
 
+            <thead className="div-myRoomsTable-thead">
+                <tr>
+                    <th>제목</th>
+                    <th>정원</th>
+                    <th>종류</th>
+                    <th>상태</th>
                 </tr>
-            )}
+            </thead>
+
+            <tbody>
+                {rooms.map((room) =>
+                    <tr key={room.no} className="div-myRoomsTable-tr">
+                        <td className="myRoomsTable-title">
+                            {room.title}
+                        </td>
+                        <td className="myRoomsTable-capacity">
+                            {room.capacity}
+                        </td>
+                        <td className="myRoomsTable-type">
+                            {room.type}
+                        </td>
+                        <td className="myRoomsTable-status">
+                            {room.status === "opened" ? "공개" : "비공개"}
+                        </td>
+
+                    </tr>
+                )}
+            </tbody>
+
+
         </table>
     </>)
 }
