8e9607157ac35d4ecfac3fb71c692c69b9fd8374
domino3333 <busster@naver.com>
2026-06-17 21:04:39 +0900

    호스트 측에서 구독하는 함수 작성

--- src/api/room/qna/questionApi.jsx
+++ src/api/room/qna/questionApi.jsx
@@ -5,11 +5,11 @@ import WebSocketHost from "../../ApiHost"
 const wsPrefix = "/app/qna";
 
 //질문 등록(publish)
-export const registerQuestion = (client, data)=>{
+export const registerQuestion = (client, data) => {
 
     client.publish({
-        destination:`${wsPrefix}/register`,
-        body:JSON.stringify(data)
+        destination: `${wsPrefix}/register`,
+        body: JSON.stringify(data)
     });
 
 }
--- src/pages/qnaRoom/QnaHost.jsx
+++ src/pages/qnaRoom/QnaHost.jsx
@@ -14,40 +14,31 @@ const QnaHost = () => {
 
 
     useEffect(() => {
+
         const client = connectWebSocket((connectedClient) => {
             clientRef.current = connectedClient;
+
+            connectedClient.subscribe(`/topic/qna/${no}`, (frame) => {
+                const data = JSON.parse(frame.body);
+                console.log("구독 data:", data);
+            });
+
         })
 
         return () => client.deactivate();
 
-    }, [])
+    }, [no])
 
 
 
 
-    const sendTest = () => {
-
-        const client = clientRef.current;
-
-        if (!client || !client.connected) {
-            return;
-        }
-
-        client.publish({
-            destination: "/app/qna/register",
-            body: JSON.stringify({
-                roomNo: Number(no),
-                message: "호스트가 보낸 메시지"
-            })
-        })
-    }
 
 
     return (<>
 
         <h1>{no}</h1>
         <p>호스트의qna 방</p>
-        <button onClick={sendTest}>테스트버튼</button>
+        <p>질문:{ }</p>
     </>)
 }
 
