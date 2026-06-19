fbdf3236aa80202467aa860d04b408d086d94a90
domino3333 <busster@naver.com>
2026-06-19 22:27:35 +0900

    RoomBodyTop 구조 잡아놓기

--- src/components/host/home/room/RoomBodyTop.jsx
+++ src/components/host/home/room/RoomBodyTop.jsx
@@ -1,10 +1,27 @@
 
+import "../../../../css/host/home/room/RoomBodyTop.css"
 
-const RoomBodyTop = ()=>{
+const RoomBodyTop = () => {
 
-    return(<>
-    
-    
+    return (<>
+
+        <div className="RoomBodyTop-main-div">
+            <div className="timer-div">
+                타이머 존
+            </div>
+            <div className="room-info-div">
+                <div className="room-info-1">
+                    방 정보1
+                </div>
+                <div className="room-info-2">
+                    방 정보2
+                </div>
+                <div className="room-info-3">
+                    방 정보3
+                </div>
+            </div>
+        </div>
+
     </>)
 }
 
