9e812c1add3d336fc1a2770092952d8121f6f41b
domino3333 <busster@naver.com>
2026-06-19 22:12:28 +0900

    RoomMiniHeader에 90% 직선을 위한 div 생성

--- src/components/host/home/room/RoomMiniHeader.jsx
+++ src/components/host/home/room/RoomMiniHeader.jsx
@@ -7,14 +7,16 @@ const RoomMiniHeader = ({ title, code, status }) => {
     return (<>
 
         <div className="RoomMiniHeader-main-div">
-            <div className="RoomMiniHeader-title">
-                방제목:{title}
-            </div>
-            <div className="RoomMiniHeader-code">
-                입장 코드:{code}
-            </div>
-            <div className="RoomMiniHeader-stauts">
-                상태:{status}
+            <div className="RoomMiniHeader-padding-div">
+                <div className="RoomMiniHeader-title">
+                    방제목:{title}
+                </div>
+                <div className="RoomMiniHeader-code">
+                    입장 코드:{code}
+                </div>
+                <div className="RoomMiniHeader-stauts">
+                    상태:dd
+                </div>
             </div>
         </div>
 
