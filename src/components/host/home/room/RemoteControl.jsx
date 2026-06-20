77e5cbc5e423e1c77aa2afc66f48f760a7a660ea
domino3333 <busster@naver.com>
2026-06-20 22:25:32 +0900

    타이머에 대한 버튼 컴포넌트화

---
+++ src/components/host/home/room/RemoteControl.jsx
@@ -0,0 +1,24 @@
+
+import "../../../../css/host/qna/RemoteControl.css"
+
+const RemoteControl= ()=>{
+
+    return(<>
+
+        <button>
+            질문 시작
+        </button>
+        <button>
+            추천 시작
+        </button>
+        <button>
+            결과 공개
+        </button>
+        <button>
+            질문 종료
+        </button>
+    </>)
+}
+
+
+export default RemoteControl;
