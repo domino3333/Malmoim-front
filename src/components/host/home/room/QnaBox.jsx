25feb94c34c653e2c2ee956767af7e30194ebd67
domino3333 <busster@naver.com>
2026-06-20 22:25:44 +0900

    질문 리스트에 들어갈 qndbox 컴포넌트화

---
+++ src/components/host/home/room/QnaBox.jsx
@@ -0,0 +1,27 @@
+
+
+import "../../../../css/host/qna/QnaBox.css"
+
+const QnaBox = ()=>{
+
+    return(<>
+        <div className="QnaBox-main-div">
+            <div className="QnaBox-header-div">
+                <div className="QnaBox-nickname-div">
+                    닉네임
+                </div>
+                <div className="QnaBox-vote-div">
+                    추천수:33
+                </div>
+            </div>
+            <div className="QnaBox-body-div">
+                <div>
+                    질문내용 한줄.......
+                </div>
+            </div>
+
+        </div>
+    </>)
+}
+
+export default QnaBox;
