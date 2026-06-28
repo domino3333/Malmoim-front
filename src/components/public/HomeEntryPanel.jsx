e62704015a6a80a307a3f5fa21583cd7cfcf764b
domino3333 <busster@naver.com>
2026-06-29 05:41:02 +0900

    observe, state 뚫어놓기

--- src/components/public/HomeEntryPanel.jsx
+++ src/components/public/HomeEntryPanel.jsx
@@ -2,9 +2,21 @@
 import "../../css/public/HomeEntryPanel.css"
 import people from "../../assets/people-icon.png"
 import home from "../../assets/home-icon.png"
+import { useState } from "react"
 
 const HomeEntryPanel = () => {
 
+
+    const [code, setCode] = useState("");
+
+
+    const observeCode = (e) => {
+        setCode(e.target.value);
+    }
+
+    const sendCode = () => {
+
+    }
 
     return (<>
 
@@ -14,8 +26,8 @@ const HomeEntryPanel = () => {
                 <h2>참여자이신가요?</h2>
                 <p className="panel-guide-text">입장코드를 입력하고 실시간 Q&A에 참여하세요</p>
                 <div className="left-panel-input-div">
-                    <input type="text" name="code" className="code-input" />
-                    <button className="code-input-arrow-button">→</button>
+                    <input onChange={observeCode} type="text" name="code" className="code-input" />
+                    <button onClick={sendCode} className="code-input-arrow-button">→</button>
                 </div>
                 <div className="left-panel-advice-div">
                     입장코드는 호스트가 제공한 코드를 입력해주세요
