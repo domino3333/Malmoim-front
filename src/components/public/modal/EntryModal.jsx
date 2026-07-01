d0de05eaedd8061df441462a063dcc1ea351ed61
domino3333 <busster@naver.com>
2026-07-01 23:26:08 +0900

    모달 css 수정

--- src/components/public/modal/EntryModal.jsx
+++ src/components/public/modal/EntryModal.jsx
@@ -6,18 +6,24 @@ const EntryModal = ({ roomInfo, show, onHide }) => {
 
     //roominfo에 roomNo, title, code ,hasPassword 내려옴
 
-    console.log("roomfinfo",roomInfo.hasPassword);
     return (<>
         <Modal show={show} contentClassName="Entry-modal">
             <div className="entry-modal-body">
+                <button className="entry-modal-x-button"
+                    onClick={onHide}
+                >X</button>
                 <h3>방 제목: {roomInfo.title}</h3>
-                <p>코드: {roomInfo.code}</p>
-                
-                {roomInfo.hasPassword && 
+                <p className="entry-modal-code-p">코드: {roomInfo.code}</p>
+
+                {roomInfo.hasPassword &&
                     <div className="entry-modal-password-div">
                         비밀번호:
                         <input type="password" />
                     </div>}
+
+                <button className="entry-modal-enter-button">
+                    입장하기
+                </button>
 
             </div>
         </Modal>
