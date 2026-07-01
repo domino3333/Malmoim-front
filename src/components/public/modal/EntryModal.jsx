825c5258a9e4016ebfe091c5e75ba53180fa0716
domino3333 <busster@naver.com>
2026-07-01 23:04:35 +0900

    code 내려오는 것 확인 완료

--- src/components/public/modal/EntryModal.jsx
+++ src/components/public/modal/EntryModal.jsx
@@ -4,14 +4,19 @@ import "../../../css/public/modal/EntryModal.css"
 
 const EntryModal = ({ roomInfo, show, onHide }) => {
 
-    //roominfo에 roomNo, title, hasPassword 내려옴
+    //roominfo에 roomNo, title, code ,hasPassword 내려옴
 
     return (<>
         <Modal show={show} contentClassName="Entry-modal">
             <div className="entry-modal-body">
                 <h3>방 제목: {roomInfo.title}</h3>
-                <p></p>
-
+                <p>코드: {roomInfo.code}</p>
+                <p>{roomInfo.hasPassword}</p>
+                {roomInfo.hasPassword && 
+                    <div className="entry-modal-password-div">
+                        비밀번호:
+                        <input type="password" />
+                    </div>}
 
             </div>
         </Modal>
