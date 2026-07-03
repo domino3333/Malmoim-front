01d5550675bdcd4ad347125eae855a3c3b16b15e
domino3333 <busster@naver.com>
2026-07-03 22:52:53 +0900

    entry modal을 재활용해서 nickname 모달 추가

---
+++ src/components/public/modal/NickNameModal.jsx
@@ -0,0 +1,29 @@
+import { Modal } from "react-bootstrap";
+import "../../../css/public/modal/NickNameModal.css"
+
+
+const NickNameModal = ({ show, onHide }) => {
+
+
+    return (<>
+        <Modal show={show} contentClassName="Nickname-modal">
+                <button className="nickname-modal-x-button"
+                    onClick={onHide}
+                >X</button>
+                <h3>{roomInfo.title}</h3>
+                <p className="nickname-modal-code-p">코드: {roomInfo.code}</p>
+                <p className="nickname-modal-capacity-p">정원: (현재정원표기 구현예정)/{roomInfo.capacity}</p>
+
+                {roomInfo.hasPassword &&
+                    <input className="nickname-modal-password-input" type="password" placeholder="비밀번호.." />
+                }
+
+                <button className={roomInfo.hasPassword ?"nickname-modal-enter-button-v1": "nickname-modal-enter-button-v2"}>
+                    입장하기
+                </button>
+
+        </Modal>
+    </>)
+}
+
+export default NickNameModal;
