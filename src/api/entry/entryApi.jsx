88ed1d74846a5ed53155a3b94938a1daeaa11358
domino3333 <busster@naver.com>
2026-07-03 23:15:43 +0900

    방 비밃번호 체크하는 함수 추가

--- src/api/entry/entryApi.jsx
+++ src/api/entry/entryApi.jsx
@@ -4,12 +4,24 @@ import { ApiHost } from "../ApiHost";
 
 const prefix = "/api/entry"
 
+// 방 코드를 확인하는 api 호출
 export const checkRoomCode = async (code) => {
 
-    const result = await axios.post(`${ApiHost}${prefix}/check-code`, {code},
+    const result = await axios.post(`${ApiHost}${prefix}/check-code`, { code },
         null
     );
 
     return result.data;
+
+}
+
+// 비밀번호를 확인하는 api 호출
+
+export const checkRoomPassword = async (password) => {
+
+    const result = await axios.post(`${ApiHost}${prefix}/check-password`, { password },
+        null
+    )
+
 
 }
