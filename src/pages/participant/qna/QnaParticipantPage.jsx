0cf3758bb5afb629b209be98a6104195d7dbc719
domino3333 <busster@naver.com>
2026-07-10 03:17:58 +0900

    주석 추가

--- src/pages/participant/qna/QnaParticipantPage.jsx
+++ src/pages/participant/qna/QnaParticipantPage.jsx
@@ -24,6 +24,7 @@ const QnaParticipantPage = () => {
     const clientRef = useRef(null);
 
 
+    // 화면 별 view에 대한 맵
     const phaseComponents = {
         READY: ParticipantReadyView,
         QUESTION_OPEN: ParticipantQuestionOpenView,
