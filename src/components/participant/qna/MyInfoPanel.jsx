7ed521b4232a8f705263b912fb68d9a7b53cb0bd
domino3333 <busster@naver.com>
2026-07-10 03:50:46 +0900

    MyInfoPanel 뼈대 잡기

--- src/components/participant/qna/MyInfoPanel.jsx
+++ src/components/participant/qna/MyInfoPanel.jsx
@@ -3,7 +3,12 @@ import "../../../css/participant/qna/MyInfoPanel.css"
 const MyInfoPanel = ()=>{
 
     return(<>
-    
+        
+        <div className="MyInfoPanel-parent-div">
+            <p>닉네임 : </p>
+            <p>내 질문 수:</p>
+            <p>추천한 질문 수:</p>
+        </div>
     </>)
 }
 
