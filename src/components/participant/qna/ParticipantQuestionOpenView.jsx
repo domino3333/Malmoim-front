02da677034ac1517ed85b227cb732d359448adab
domino3333 <busster@naver.com>
2026-07-13 20:32:04 +0900

    내 정보 패널, 참여자 리스트를 보여주는 패널은 이전에 만들었던 컴포넌트로 갖다 쓰기

--- src/components/participant/qna/ParticipantQuestionOpenView.jsx
+++ src/components/participant/qna/ParticipantQuestionOpenView.jsx
@@ -1,5 +1,7 @@
 
 import "../../../css/participant/qna/ParticipantQuestionOpenView.css"
+import ParticipantListPanel from "../ParticipantListPanel";
+import MyInfoPanel from "./MyInfoPanel";
 import StatusPanel from "./StatusPanel";
 import TimerPanel from "./TimerPanel";
 
@@ -7,8 +9,18 @@ const ParticipantQuestionOpenView = () => {
 
     return (<>
         <div className="Question-open-view-head">
-            <TimerPanel/>
-            <StatusPanel/>
+            <TimerPanel />
+            <StatusPanel />
+
+        </div>
+        <div className="Question-open-view-body">
+            <div className="question-left-panel">
+                질문 등록할 수 있는 박스
+            </div>
+            <div className="question-right-panel">
+                <MyInfoPanel/>
+                <ParticipantListPanel/>
+            </div>
         </div>
     </>)
 }
