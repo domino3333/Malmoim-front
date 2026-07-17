e94d5a3f96560d839784980da2bd8fa88d34f635
domino3333 <busster@naver.com>
2026-07-17 23:54:11 +0900

    타이머 인포 잘 내려오는지 확인하기

--- src/components/participant/qna/TimerPanel.jsx
+++ src/components/participant/qna/TimerPanel.jsx
@@ -3,11 +3,14 @@ import "./../../../css/participant/qna/TimerPanel.css"
 
 const TimerPanel = ({ timerInfo }) => {
 
+
+    
+
     return (<>
 
         <div className="timer-panel-parent-div">
-            타이머존
-
+            {timerInfo.questionStartedAt}
+            <p>종료:{timerInfo.questionEndedAt}</p>
         </div>
     </>)
 }
