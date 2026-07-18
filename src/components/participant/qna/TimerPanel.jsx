47c13bc245a0d91d77d2582fec87dccf9f030e29
domino3333 <busster@naver.com>
2026-07-18 22:45:08 +0900

    호스트가 질문을 시작했을 때 참여자 페이지 자동 변환 및 타이머 시작 완료

--- src/components/participant/qna/TimerPanel.jsx
+++ src/components/participant/qna/TimerPanel.jsx
@@ -1,17 +1,48 @@
+import { useTimer } from "react-timer-hook"
 import "./../../../css/participant/qna/TimerPanel.css"
+import { useEffect } from "react"
 
 
 const TimerPanel = ({ timerInfo }) => {
 
+    const {
+        seconds,
+        minutes,
+        restart,
+        isRunning
+    } = useTimer({
+        expiryTimestamp:new Date(),
+        autoStart:false,
+        onExpire:()=>{
+            console.log("질문 시간 종료");
+        }
 
-    //todo 시작/종료시간으로 타이머 그리기
+    })
+
+
+    useEffect(()=>{
+        if(!timerInfo?.questionEndedAt){
+            return;
+        }
+
+        const expiryTime = new Date(timerInfo.questionEndedAt);
+
+        restart(expiryTime,true);
+
+    },[timerInfo?.questionEndedAt]);
+
+
 
     return (<>
 
         <div className="timer-panel-parent-div">
-            <p>시작{timerInfo.questionStartedAt}</p>
-            <p>종료:{timerInfo.questionEndedAt}</p>
+            <p>
+                {String(minutes).padStart(2,"0")}:
+                {String(seconds).padStart(2,"0")}
+            </p>
         </div>
+
+        <p>{isRunning ? "진행 중": "대기 중"}</p>
     </>)
 }
 
