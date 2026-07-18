087b7a5e10b45cd4f0e8834c05d31328b3888703
domino3333 <busster@naver.com>
2026-07-18 23:24:45 +0900

    타임 패널 css 수정

--- src/components/participant/qna/TimerPanel.jsx
+++ src/components/participant/qna/TimerPanel.jsx
@@ -39,7 +39,6 @@ const TimerPanel = ({ timerInfo }) => {
                 {String(minutes).padStart(2, "0")}:
                 {String(seconds).padStart(2, "0")}
             </p>
-            <p>{isRunning ? "진행 중" : "대기 중"}</p>
         </div>
 
 
