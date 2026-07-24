685f24679ac2ad56b42cca063c20e6bc136887ff
domino3333 <busster@naver.com>
2026-07-25 03:12:44 +0900

    모달 버튼 추가

--- src/components/participant/qna/modal/QuestionSubmitModal.jsx
+++ src/components/participant/qna/modal/QuestionSubmitModal.jsx
@@ -1,4 +1,5 @@
 import { Modal } from "react-bootstrap";
+import "../../../../css/participant/qna/QuestionSubmitModal.css"
 
 
 
@@ -9,8 +10,10 @@ const QuestionSubmitModal = ({show, onHide, onSubmit})=>{
     
 
         <Modal show={show} contentClassName="question-submit-modal">
+            <button>X</button>
             <h3>질문 등록</h3>
-            <textarea name="question-area">dff</textarea>
+            <textarea name="question-area" placeholder="질문을 해보세요!"/>
+            <button className="">취소</button>
             <button>완료</button>
         </Modal>
     </>)
