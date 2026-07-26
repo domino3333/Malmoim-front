896a06741e3cb4b4dfa5854d3c0ce5c149d8b6a7
domino3333 <busster@naver.com>
2026-07-26 15:27:51 +0900

    버튼을 박스로 묶어서 관리하기

--- src/components/participant/qna/modal/QuestionSubmitModal.jsx
+++ src/components/participant/qna/modal/QuestionSubmitModal.jsx
@@ -3,18 +3,22 @@ import "../../../../css/participant/qna/QuestionSubmitModal.css"
 
 
 
-const QuestionSubmitModal = ({show, onHide, onSubmit})=>{
+const QuestionSubmitModal = ({ show, onHide, onSubmit }) => {
 
 
-    return(<>
-    
+    return (<>
+
 
         <Modal show={show} contentClassName="question-submit-modal">
             <button className="question-submit-modal-x-button">X</button>
             <h3>질문 등록</h3>
-            <textarea className="qustion-submit-modal-textarea" name="question-area" placeholder="질문을 해보세요!"/>
-            <button className="">취소</button>
-            <button>완료</button>
+            <textarea className="qustion-submit-modal-textarea" name="question-area" placeholder="질문을 해보세요!" />
+
+            <div className="qustion-submit-modal-button-box">
+                <button className="qustion-submit-modal-cancel-button">취소</button>
+                <button className="qustion-submit-modal-submit-button">완료</button>
+
+            </div>
         </Modal>
     </>)
 }
