9e716b6f9cce7265f112901c166b399ac4c772e5
domino3333 <busster@naver.com>
2026-07-26 15:18:08 +0900

    질문 등록 모달 css 수정

--- src/components/participant/qna/modal/QuestionSubmitModal.jsx
+++ src/components/participant/qna/modal/QuestionSubmitModal.jsx
@@ -10,7 +10,7 @@ const QuestionSubmitModal = ({show, onHide, onSubmit})=>{
     
 
         <Modal show={show} contentClassName="question-submit-modal">
-            <button>X</button>
+            <button className="question-submit-modal-x-button">X</button>
             <h3>질문 등록</h3>
             <textarea name="question-area" placeholder="질문을 해보세요!"/>
             <button className="">취소</button>
