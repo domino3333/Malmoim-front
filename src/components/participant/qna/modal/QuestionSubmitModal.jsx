101eadb1b26d92b41544d4f773085df038690036
domino3333 <busster@naver.com>
2026-07-26 15:25:55 +0900

    textarea css 수정

--- src/components/participant/qna/modal/QuestionSubmitModal.jsx
+++ src/components/participant/qna/modal/QuestionSubmitModal.jsx
@@ -12,7 +12,7 @@ const QuestionSubmitModal = ({show, onHide, onSubmit})=>{
         <Modal show={show} contentClassName="question-submit-modal">
             <button className="question-submit-modal-x-button">X</button>
             <h3>질문 등록</h3>
-            <textarea name="question-area" placeholder="질문을 해보세요!"/>
+            <textarea className="qustion-submit-modal-textarea" name="question-area" placeholder="질문을 해보세요!"/>
             <button className="">취소</button>
             <button>완료</button>
         </Modal>
