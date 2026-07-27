55e3f4494a0ea7d0f939290dde2c406a71ed9e6b
domino3333 <busster@naver.com>
2026-07-27 23:01:57 +0900

    주석 추가

--- src/api/room/qna/socketApi.jsx
+++ src/api/room/qna/socketApi.jsx
@@ -7,6 +7,7 @@ const wsPrefix = "/app/qna";
 // 작성한 질문을 Q&A 서버 목적지로 발행.
 export const publishQuestion = (client, data) => {
 
+    // data가 곧 dto임
     client.publish({
         destination: `${wsPrefix}/register`,
         body: JSON.stringify(data)
