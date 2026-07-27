ed0a7021708a5a03b8d953d7e0eceda234033d87
domino3333 <busster@naver.com>
2026-07-27 22:47:57 +0900

    textarea에 onChange 함수 추가

--- src/components/participant/qna/modal/QuestionSubmitModal.jsx
+++ src/components/participant/qna/modal/QuestionSubmitModal.jsx
@@ -1,22 +1,31 @@
 import { Modal } from "react-bootstrap";
 import "../../../../css/participant/qna/QuestionSubmitModal.css"
+import { useState } from "react";
 
 
 
 const QuestionSubmitModal = ({ show, onHide, onSubmit }) => {
 
 
+    const [question,setQuestion] = useState("");
+
+
+    const handleQuestionChange = (e)=>{
+        setQuestion(e.target.value);
+    }
     return (<>
 
 
         <Modal show={show} contentClassName="question-submit-modal">
             <button className="question-submit-modal-x-button">X</button>
             <h3>질문 등록</h3>
-            <textarea className="qustion-submit-modal-textarea" name="question-area" placeholder="질문을 등록해 보세요!" />
+            <textarea onChange={handleQuestionChange} className="qustion-submit-modal-textarea" name="question-area" placeholder="질문을 등록해 보세요!" />
 
             <div className="qustion-submit-modal-button-box">
                 <button className="qustion-submit-modal-cancel-button">취소</button>
-                <button className="qustion-submit-modal-submit-button">완료</button>
+                <button className="qustion-submit-modal-submit-button"
+                    onClick={onSubmit}
+                >완료</button>
 
             </div>
         </Modal>
