5ca443c1f4210c3013cad81513434a054cc33962
domino3333 <busster@naver.com>
2026-07-30 12:24:53 +0800

    QnA 이름 표기법 정리

--- src/pages/host/home/CreateDetailPage.jsx
+++ src/pages/host/home/CreateDetailPage.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import HostHomeHeader from "../../../components/host/home/HostHomeHeader";
-import HostHomeSideBar from "../../../components/host/home/HostHomeSideBar";
+import HostHomeSidebar from "../../../components/host/home/HostHomeSidebar";
 import "../../../css/host/home/CreateDetailPage.css";
 import audience from "../../../assets/audience.png"
 import { useState } from "react";
@@ -20,13 +20,13 @@ const CreateDetailPage = () => {
 
         {/*대쉬보드 전체를 감싸는 body div*/}
         <div className="div-CreateDetailPage-body">
-            <HostHomeSideBar/>
+            <HostHomeSidebar/>
             <div className="div-CreateDetailPage-content-list">
                 <div className="btn-box">
                     <button className="btn1" onClick={() => setIsCreateModalOpen(true)}>
                         <img src={audience} alt="청중이미지" />
                         <div className="div-qna">
-                            청중 QnA
+                            청중 Q&A
                         </div>
                     </button>
 
@@ -34,7 +34,7 @@ const CreateDetailPage = () => {
             </div>
         </div>
 
-        <CreateQnaRoomModal show={isCreateModalOpen} onHide={()=>setIsCreateModalOpen(false)} title={"청중QnA"}/>
+        <CreateQnaRoomModal show={isCreateModalOpen} onHide={()=>setIsCreateModalOpen(false)} title={"청중Q&A"}/>
 
         
 
