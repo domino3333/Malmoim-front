0935a3aeee37582b6320bf27bef7e268588fbb76
domino3333 <busster@naver.com>
2026-08-02 14:08:22 +0900

    css 관계 수정

--- src/components/participant/qna/ParticipantQuestionList.jsx
+++ src/components/participant/qna/ParticipantQuestionList.jsx
@@ -5,7 +5,7 @@ const ParticipantQuestionList = ({ onClickRegister}) => {
     return (<>
 
         <p>질문 리스트</p>
-        <button onClick={onClickRegister} className="question-left-panel-register">등록하기</button>
+        <button onClick={onClickRegister} className="question-register">등록하기</button>
 
         
     </>)
