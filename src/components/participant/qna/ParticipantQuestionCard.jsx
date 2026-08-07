ef6fcbd49465cbe42942184f1ba7c7045754fc42
domino3333 <busster@naver.com>
2026-08-07 23:15:48 +0900

    참여자 질문 카드 css 수정

--- src/components/participant/qna/ParticipantQuestionCard.jsx
+++ src/components/participant/qna/ParticipantQuestionCard.jsx
@@ -4,13 +4,18 @@ const ParticipantQuestionCard = ({ questions }) => {
 
     return (<>
 
+        <div className="ParticipantQuestionCard-list">
 
-        {questions.map((question, i) => 
-            <div key={question.no} className="ParticipantQuestionCard">
-                <p className="ParticipantQuestionCard-nickname">닉네임:{question.nickname}</p>
-                <p className="ParticipantQuestionCard-content">{question.content}</p>
-            </div>
-        )}
+
+
+            {questions.map((question, i) =>
+                <div key={question.no} className="ParticipantQuestionCard">
+                    <p className="ParticipantQuestionCard-nickname">닉네임:{question.nickname}</p>
+                    <p className="ParticipantQuestionCard-content">{question.content}</p>
+                </div>
+            )}
+
+        </div>
     </>)
 }
 
