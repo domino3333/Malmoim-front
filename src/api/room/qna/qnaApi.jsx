8305e772936c52016073ba7d15c01f6c14b7de23
domino3333 <busster@naver.com>
2026-08-08 17:49:32 +0900

    참여자 토큰에 이미 roomNo가 있어서 굳이 넘길 필요 없을 듯

--- src/api/room/qna/qnaApi.jsx
+++ src/api/room/qna/qnaApi.jsx
@@ -77,13 +77,15 @@ export const updateRoomStatus = async (roomNo, status) => {
 
 
 //참여자의 정보를 받아오는 api
-export const getParticipantInfo = async (roomNo) => {
+export const getParticipantInfo = async () => {
 
     const token = sessionStorage.getItem(`malmoim:participant-session:${roomNo}`);
 
-    const response = await axios.get(`${API_BASE_URL}${prefix}/${roomNo}/participant-info`, {
+    const response = await axios.get(`${API_BASE_URL}${prefix}/participant-info`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
     })
+
+    return response.data;
 }
