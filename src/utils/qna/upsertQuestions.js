0b0f656741c206f1acc4ff5019d63b8e3cd1c509
domino3333 <busster@naver.com>
2026-08-18 10:23:29 +0900

    이전 질문과 새로운 질문 중 중복된 질문을 제거하는 병합 함수 추가

---
+++ src/utils/qna/upsertQuestions.js
@@ -0,0 +1,19 @@
+
+const mergeQuestionLists = (baseQuestions,latestQuestions) =>{
+
+    const questionMap = new Map();
+
+    baseQuestions.forEach(question => {
+        questionMap.set(question.no, question);
+    });
+
+    latestQuestions.forEach(question =>{
+        questionMap.set(question.no,question);
+    });
+
+    return Array.from(questionMap.values())
+        .sort((a,b) =>
+            new Date(a.createdAt).getTime()
+            - new Date(b.createdAt).getTime()
+        );
+}
