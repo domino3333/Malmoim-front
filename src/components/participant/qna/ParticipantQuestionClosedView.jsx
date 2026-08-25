fa631799cf366ecf1e0c538c18c3c128700891ed
domino3333 <busster@naver.com>
2026-08-25 13:45:36 +0900

    질문 종료 페이지 구상 및 css 수정

--- src/components/participant/qna/ParticipantQuestionClosedView.jsx
+++ src/components/participant/qna/ParticipantQuestionClosedView.jsx
@@ -4,14 +4,19 @@ const ParticipantQuestionClosedView = () => {
 
 
     return (<>
-        <div className="ParticipantQuestionClosedView-parent-div">
-                <div className="ParticipantQuestionClosedView-left-panel">
+            <div className="ParticipantQuestionClosedView-parent-panel">
+                <div className="ParticipantQuestionClosedView-top-panel">
                     질문 시간이 종료되었어요.
-                    <br/>
+                    <br />
+                    질문들을 미리 읽고
+                    <br />
                     투표가 시작되면 공감하는 질문에 좋아요를 눌러주세요.
                 </div>
-                
+                <div className="ParticipantQuestionClosedView-bottom-panel">
+                    ff
+                </div>
             </div>
+
     </>)
 }
 
