2b5c56b0e4b4cba032404298a5744ea20a10501c
domino3333 <busster@naver.com>
2026-08-25 13:36:17 +0900

    패널 텍스트 변경

--- src/components/participant/qna/ParticipantQuestionClosedView.jsx
+++ src/components/participant/qna/ParticipantQuestionClosedView.jsx
@@ -7,6 +7,8 @@ const ParticipantQuestionClosedView = () => {
         <div className="ParticipantQuestionClosedView-parent-div">
                 <div className="ParticipantQuestionClosedView-left-panel">
                     질문 시간이 종료되었어요.
+                    <br/>
+                    투표가 시작되면 공감하는 질문에 좋아요를 눌러주세요.
                 </div>
                 
             </div>
