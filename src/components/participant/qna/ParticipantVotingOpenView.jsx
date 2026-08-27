8b8e7caa8974d63c15b872aade0c4cd8bb0b35dc
domino3333 <busster@naver.com>
2026-08-27 23:52:09 +0900

    임시 주석 처리, questions 내려 받아야 함

--- src/components/participant/qna/ParticipantVotingOpenView.jsx
+++ src/components/participant/qna/ParticipantVotingOpenView.jsx
@@ -1,11 +1,12 @@
 
 import "../../../css/participant/qna/ParticipantVotingOpenView.css"
+import ParticipantQuestionCard from "./ParticipantQuestionCard";
 const ParticipantVotingOpenView = () => {
 
     return (<>
         <div className="ParticipantVotingOpenView-parent-div">
                 <div className="ParticipantVotingOpenView-left-panel">
-                    투표 오픈
+                    {/*<ParticipantQuestionCard/>*/}
                 </div>
                 
             </div>
