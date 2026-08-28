7603dc3924f87c39c2bd13d161aaeff431620adb
domino3333 <busster@naver.com>
2026-08-28 20:28:37 +0900

    투표 시작 view css 수정

--- src/components/participant/qna/ParticipantVotingOpenView.jsx
+++ src/components/participant/qna/ParticipantVotingOpenView.jsx
@@ -4,12 +4,18 @@ import ParticipantQuestionCard from "./ParticipantQuestionCard";
 const ParticipantVotingOpenView = ({ questions }) => {
 
     return (<>
-        <div className="ParticipantVotingOpenView-parent-div">
-            <div className="ParticipantVotingOpenView-left-panel">
-                <ParticipantQuestionCard questions={questions} />
+         <div className="ParticipantVotingOpenView-parent-panel">
+                <div className="ParticipantVotingOpenView-top-panel">
+                    투표가 시작되었어요.
+                    <br />
+                    질문들을 읽고
+                    <br />
+                    공감하는 질문에 좋아요를 눌러주세요.
+                </div>
+                <div className="ParticipantVotingOpenView-bottom-panel">
+                    <ParticipantQuestionCard questions={questions}/>
+                </div>
             </div>
-
-        </div>
     </>)
 }
 
