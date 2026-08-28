bf2faeeddf6089cd77d5180abe59a0cd862e8b51
domino3333 <busster@naver.com>
2026-08-28 20:31:53 +0900

    ParticipantVotingClosedView css 수정

--- src/components/participant/qna/ParticipantVotingClosedView.jsx
+++ src/components/participant/qna/ParticipantVotingClosedView.jsx
@@ -1,17 +1,18 @@
 import ParticipantQuestionCard from "./ParticipantQuestionCard";
+import "../../../css/participant/qna/ParticipantVotingClosedView.css"
 
 
 const ParticipantVotingClosedView = ({questions}) => {
 
 
     return (<>
-        <div className="ParticipantVotingOpenView-parent-panel">
-                <div className="ParticipantVotingOpenView-top-panel">
+        <div className="ParticipantVotingClosedView-parent-panel">
+                <div className="ParticipantVotingClosedView-top-panel">
                     투표가 종료되었어요.
                     <br />
                     호스트가 여러분들의 질문에 대답할 차례예요
                 </div>
-                <div className="ParticipantVotingOpenView-bottom-panel">
+                <div className="ParticipantVotingClosedView-bottom-panel">
                     <ParticipantQuestionCard questions={questions}/>
                 </div>
             </div>
