24fd4b4bd3019ff028bd4a7081a74647b6805413
domino3333 <busster@naver.com>
2026-08-28 20:30:35 +0900

    ParticipantVotingClosedView 뼈대 잡기 완료

--- src/components/participant/qna/ParticipantVotingClosedView.jsx
+++ src/components/participant/qna/ParticipantVotingClosedView.jsx
@@ -1,10 +1,20 @@
+import ParticipantQuestionCard from "./ParticipantQuestionCard";
 
 
-const ParticipantVotingClosedView = () => {
+const ParticipantVotingClosedView = ({questions}) => {
 
 
     return (<>
-        <p>ParticipantVotingClosedView</p>
+        <div className="ParticipantVotingOpenView-parent-panel">
+                <div className="ParticipantVotingOpenView-top-panel">
+                    투표가 종료되었어요.
+                    <br />
+                    호스트가 여러분들의 질문에 대답할 차례예요
+                </div>
+                <div className="ParticipantVotingOpenView-bottom-panel">
+                    <ParticipantQuestionCard questions={questions}/>
+                </div>
+            </div>
     </>)
 }
 
