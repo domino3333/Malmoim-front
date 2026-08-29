2f8a6f446899eb068217ef19637f5fd91ee5eff0
domino3333 <busster@naver.com>
2026-08-29 20:21:23 +0900

    최상위 부모인 qna participant page보단 투표 오픈view가 투표 클릭에 대한 책임을 갖도록 수정

--- src/components/participant/qna/ParticipantVotingOpenView.jsx
+++ src/components/participant/qna/ParticipantVotingOpenView.jsx
@@ -3,19 +3,30 @@ import "../../../css/participant/qna/ParticipantVotingOpenView.css"
 import ParticipantQuestionCard from "./ParticipantQuestionCard";
 const ParticipantVotingOpenView = ({ questions }) => {
 
+
+
+    const handleVote = async (questionNo) => {
+        await voteQuestion(questionNo);
+
+    }
+
     return (<>
-         <div className="ParticipantVotingOpenView-parent-panel">
-                <div className="ParticipantVotingOpenView-top-panel">
-                    투표가 시작되었어요.
-                    <br />
-                    질문들을 읽고
-                    <br />
-                    공감하는 질문에 좋아요를 눌러주세요.
-                </div>
-                <div className="ParticipantVotingOpenView-bottom-panel">
-                    <ParticipantQuestionCard questions={questions} canVote={true}/>
-                </div>
+        <div className="ParticipantVotingOpenView-parent-panel">
+            <div className="ParticipantVotingOpenView-top-panel">
+                투표가 시작되었어요.
+                <br />
+                질문들을 읽고
+                <br />
+                공감하는 질문에 좋아요를 눌러주세요.
             </div>
+            <div className="ParticipantVotingOpenView-bottom-panel">
+                <ParticipantQuestionCard
+                    questions={questions}
+                    canVote={true}
+                    onVote={handleVote}
+                />
+            </div>
+        </div>
     </>)
 }
 
