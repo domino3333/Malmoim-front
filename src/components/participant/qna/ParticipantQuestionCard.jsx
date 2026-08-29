ed1d0c73d2b79f0dec72cc8fa7b58a61392937f1
domino3333 <busster@naver.com>
2026-08-29 20:11:37 +0900

    좋아요 버튼을 넣기 위한 html 구성 수정

--- src/components/participant/qna/ParticipantQuestionCard.jsx
+++ src/components/participant/qna/ParticipantQuestionCard.jsx
@@ -4,18 +4,23 @@ const ParticipantQuestionCard = ({ questions }) => {
 
     return (<>
 
-        <div className="ParticipantQuestionCard-list">
 
-            {questions.map((question, i) =>
-                <div key={question.no} className="ParticipantQuestionCard">
+        {questions.map((question, i) =>
+            <div key={question.no} className="ParticipantQuestionCard">
+                <div className="ParticipantQuestionCard-left">
                     <p className="ParticipantQuestionCard-nickname">닉네임:{question.nickname}</p>
                     <p className="ParticipantQuestionCard-content">{question.content}</p>
                     <p className="ParticipantQuestionCard-time">{question.createdAt}</p>
-                    
                 </div>
-            )}
+                <div className="ParticipantQuestionCard-right">
+                    <button>
+                        좋아요
+                    </button>
+                </div>
 
-        </div>
+            </div>
+        )}
+
     </>)
 }
 
