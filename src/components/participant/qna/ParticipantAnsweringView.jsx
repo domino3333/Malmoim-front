a57fbe005eaffbca47791948505355541b9bda52
domino3333 <busster@naver.com>
2026-08-30 23:06:41 +0900

    '결과 공개' 버튼에 대한 화면은 answering view로 설정하고 뼈대 잡아놓기

--- src/components/participant/qna/ParticipantAnsweringView.jsx
+++ src/components/participant/qna/ParticipantAnsweringView.jsx
@@ -1,9 +1,19 @@
 
-const ParticipantAnsweringView = ()=>{
+import "../../../css/participant/qna/ParticipantAnsweringView.css"
 
-    return(<>
-    <p> 답변화면 </p>
-    
+const ParticipantAnsweringView = () => {
+
+    return (<>
+        <div className="ParticipantAnsweringView-parent-panel">
+            <div className="ParticipantAnsweringView-top-panel">
+                좋아요 순서대로 답변이 정렬되었어요.
+                <br />
+                호스트가 질문에 답변할 차례예요.
+            </div>
+            <div className="ParticipantAnsweringView-bottom-panel">
+            </div>
+        </div>
+
     </>)
 }
 
