d564a7753ee50f0f13ee5974e5047c0d53e8099a
domino3333 <busster@naver.com>
2026-09-16 15:46:42 +0900

    person 아이콘에 드롭다운 메뉴 적용

--- src/components/host/home/HostHomeHeader.jsx
+++ src/components/host/home/HostHomeHeader.jsx
@@ -2,21 +2,43 @@
 import { useNavigate } from "react-router-dom"
 import person from "../../../assets/person.png"
 import "../../../css/host/home/HostHomeHeader.css"
+import { Dropdown } from "react-bootstrap";
 
 //host 페이지의 상단 바
 const HostHomeHeader = () => {
 
     const nav = useNavigate();
 
+    const handleLogout = () => {
+        sessionStorage.removeItem("accessToken");
+        nav("/");
+    }
+
     return (
         <header className="host-home-header">
-            <button
-                type="button"
-                className="host-home-header-person-button"
-                onClick={() => nav("/")}
-            >
-                <img src={person} alt="내 정보" />
-            </button>
+
+            <Dropdown align="end" className="host-account-dropdown">
+                <Dropdown.Toggle
+                    variant="link"
+                    className="host-home-header-person-button"
+                >
+                    <img src={person} alt="내 정보" />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item onClick={()=>nav("/dashboard")}>
+                        대시보드
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={()=>nav("/myContent")}>
+                        내 말모임
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={handleLogout}>
+                        로그아웃
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+
+            </Dropdown>
+
+
         </header>
     )
 }
