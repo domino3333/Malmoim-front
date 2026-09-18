98308339ce36e7b65edbc05180c390e7ef99222e
domino3333 <busster@naver.com>
2026-09-18 16:30:18 +0900

    QnaPhaseStatus 추가

---
+++ src/utils/qna/ColumnStatus.js
@@ -0,0 +1,19 @@
+
+
+export const QuestionStatus = {
+    WAITING:"대기 중",
+    ANSWERED:"답변 완료",
+    REJECTED:"거절됨",
+    HIDDEN:"숨겨진 답변"
+}
+
+export const QnaPhaseStatus = {
+    READY: "준비 중",
+    QUESTION_OPEN: "질문 접수 중",
+    QUESTION_CLOSED: "질문 접수 종료",
+    VOTING_OPEN: "투표 진행 중",
+    VOTING_CLOSED: "투표 종료",
+    ANSWERING: "결과 공개",
+    FINISHED: "종료"
+};
+
--- src/utils/qna/QuestionStatus.js
+++
@@ -1,8 +0,0 @@
-
-
-export const QuestionStatus = {
-    WAITING:"대기 중",
-    ANSWERED:"답변 완료",
-    REJECTED:"거절됨",
-    HIDDEN:"숨겨진 답변"
-}
