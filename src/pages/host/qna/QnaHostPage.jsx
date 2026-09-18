a993eb54a8431ce2ac43fe52b22368087151fb5e
domino3333 <busster@naver.com>
2026-09-18 11:35:25 +0900

    프론트에서 구독이 잘 되는지 테스트

--- src/pages/host/qna/QnaHostPage.jsx
+++ src/pages/host/qna/QnaHostPage.jsx
@@ -66,8 +66,8 @@ const QnaHostPage = () => {
     // 질문 모달에서 '답변 완료로 표시 클릭'
     const handleQuestionAnswering = async (roomNo,questionNo)=>{
         //답변 완료를 누르면 상태를 변경함과 동시에 이걸 웹소켓으로 내려줘야함
+        // http 로 응답은 받지만 http응답은 필요 없고 웹소켓으로 구독하고 데이터를 변경할거임
         const data = await completeAnswer(roomNo,questionNo);
-
 
     }
 
@@ -140,6 +140,12 @@ const QnaHostPage = () => {
                     status: data.status
                 }))
                 setTimerInfo(data);
+            });
+            
+            connectedClient.subscribe(`/topic/qna/${roomNo}/complete`, (frame) => {
+                const data = JSON.parse(frame.body);
+
+                [...questions].find((item)=>item.no === data.questionNo).status = data.status;
             });
 
             const questionListSnapshot = await getQuestionList(roomNo);
