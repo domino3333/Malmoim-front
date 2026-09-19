6a306126fdebf2b7fe23e51c2e9e4a10dbc9c475
domino3333 <busster@naver.com>
2026-09-19 17:38:17 +0900

    순위 박스 css 수정

--- src/components/host/qna/HostQuestionCard.jsx
+++ src/components/host/qna/HostQuestionCard.jsx
@@ -7,25 +7,32 @@ import { QuestionStatus } from "../../../utils/qna/ColumnStatus";
 const HostQuestionCard = ({ question, onClickCard }) => {
 
     return (<>
-        <div className="HostQuestionCard-main-div" onClick={()=>onClickCard(question)}>
-            <div className="HostQuestionCard-header-div">
-                <div className="HostQuestionCard-nickname-div">
-                    답변상태: {QuestionStatus[question.status] ?? question.status}
-                </div>
-                <div className="HostQuestionCard-vote-div">
-                    추천수: {question.voteCount}
-                </div>
-            </div>
-            <div className="HostQuestionCard-body-div">
-                <div className="HostQuestionCard-body-question">
-                    질문: {question.content}
-                </div>
-                <div className="HostQuestionCard-body-time">
-                    {question.createdAt}
-                </div>
-            </div>
 
+        <div className="HostQuestionCard-main-parent">
+            <div className="HostQuestionCard-rank-div">
+                {question.voteRank}
+            </div>
+            <div className="HostQuestionCard-main-div" onClick={() => onClickCard(question)}>
+                <div className="HostQuestionCard-header-div">
+                    <div className="HostQuestionCard-nickname-div">
+                        답변상태: {QuestionStatus[question.status] ?? question.status}
+                    </div>
+                    <div className="HostQuestionCard-vote-div">
+                        추천수: {question.voteCount}
+                    </div>
+                </div>
+                <div className="HostQuestionCard-body-div">
+                    <div className="HostQuestionCard-body-question">
+                        질문: {question.content}
+                    </div>
+                    <div className="HostQuestionCard-body-time">
+                        {question.createdAt}
+                    </div>
+                </div>
+
+            </div>
         </div>
+
     </>)
 }
 
