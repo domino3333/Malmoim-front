f4111b6a15c603802b9ad1754c4d17a2b6f01678
domino3333 <busster@naver.com>
2026-09-19 16:32:04 +0900

    질문 리스트 필터를 위한 classname 조건 추가

--- src/components/host/qna/HostQuestionList.jsx
+++ src/components/host/qna/HostQuestionList.jsx
@@ -4,11 +4,13 @@ import { useState } from "react";
 import "../../../css/host/qna/HostQuestionList.css"
 import HostQuestionCard from "./HostQuestionCard";
 
-const HostQuestionList = ({ questions,onClickCard }) => {
+const HostQuestionList = ({ questions, onClickCard }) => {
 
 
 
     const [sortOrder, setSortOrder] = useState("latest");
+
+    const [statusFilter, setStatusFilter] = useState("ALL");
 
 
 
@@ -27,13 +29,20 @@ const HostQuestionList = ({ questions,onClickCard }) => {
             <h3>질문 리스트</h3>
             <div className="qna-toolbar-main-div">
                 <div className="qna-toolbar-left-div">
-                    <button className="qna-toolbar-left-btn">
+                    <button
+                        className={`qna-toolbar-left-btn${statusFilter === "ALL" ? "isActive" : ""}`}
+                        onClick={() => setStatusFilter("ALL")}
+                    >
                         전체
                     </button>
-                    <button className="qna-toolbar-left-btn">
+                    <button
+                        className={`qna-toolbar-left-btn${statusFilter === "WAITING" ? "isActive" : ""}`}
+                        onClick={() => setStatusFilter("WAITING")}>
                         대기
                     </button>
-                    <button className="qna-toolbar-left-btn">
+                    <button
+                        className={`qna-toolbar-left-btn${statusFilter === "ANSWERED" ? "isActive" : ""}`}
+                        onClick={() => setStatusFilter("ANSWERED")}>
                         답변 완료
                     </button>
                 </div>
