0217cecc6532673357e99e29dad0861c0f3204a0
domino3333 <busster@naver.com>
2026-09-20 19:39:03 +0900

    voteCount 페이즈별 show flag 추가

--- src/utils/qna/questionListUiByPhase.js
+++ src/utils/qna/questionListUiByPhase.js
@@ -2,36 +2,43 @@ export const questionListUiByPhase = {
     READY: {
         showRank: false,
         showStatusFilter: false,
+        showVoteCount:false,
         defaultSort: "latest"
     },
     QUESTION_OPEN: {
         showRank: false,
         showStatusFilter: false,
+        showVoteCount:false,
         defaultSort: "latest"
     },
     QUESTION_CLOSED: {
         showRank: false,
         showStatusFilter: false,
+        showVoteCount:false,
         defaultSort: "latest"
     },
     VOTING_OPEN: {
         showRank: false,
         showStatusFilter: false,
+        showVoteCount:false,
         defaultSort: "latest"
     },
     VOTING_CLOSED: {
         showRank: false,
         showStatusFilter: false,
+        showVoteCount:false,
         defaultSort: "votes"
     },
     ANSWERING: {
         showRank: true,
         showStatusFilter: true,
+        showVoteCount:true,
         defaultSort: "votes"
     },
     FINISHED: {
         showRank: true,
         showStatusFilter: true,
+        showVoteCount:true,
         defaultSort: "votes"
     }
 };
