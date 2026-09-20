d6963703ec5f1fce7112bb60869bf64e053f2aed
domino3333 <busster@naver.com>
2026-09-20 19:23:19 +0900

    ready view에 넣을 이미지 고려하기

--- src/components/participant/qna/ParticipantReadyView.jsx
+++ src/components/participant/qna/ParticipantReadyView.jsx
@@ -8,7 +8,7 @@ const ParticipantReadyView = () => {
         
             <div className="ParticipantReadyView-parent-div">
                 <div className="readyView-left-panel">
-                    호스트가 질문 시간을 준비 중이에요...
+                    호스트가 질문 시간을 준비 중이에요...{/*가이드 이미지 추가 예정*/}
                 </div>
                 
             </div>
