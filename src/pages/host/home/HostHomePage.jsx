dd0e56fadfc162f11503587494b0dd0f025e59a8
domino3333 <busster@naver.com>
2026-09-22 22:01:32 +0900

    테이블 구조 잡고 css 수정

--- src/pages/host/home/HostHomePage.jsx
+++ src/pages/host/home/HostHomePage.jsx
@@ -18,13 +18,17 @@ const HostHomePage = () => {
                     <h2>최근 만든 말모임</h2>
                     <div>전체보기 → </div>
                 </div>
-                <div className="host-dashboard-content-list">
+                <div className="host-dashboard-content-list-table">
                     <div className="host-dashboard-content-list-header">
                         <p>방 제목</p>
                         <p>유형</p>
                         <p>입장 코드</p>
                         <p>생성일</p>
                     </div>
+                    <div className="host-dashboard-content-list-body">
+                        {/* map으로 돌려서 다섯 줄 만들기 */}
+                    </div>
+
                 </div>
             </div>
         </HostHomeLayout>
