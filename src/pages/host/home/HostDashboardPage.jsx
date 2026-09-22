2aab9d1356f3fbdca56e413a92d68bf7cc036fcd
domino3333 <busster@naver.com>
2026-09-23 07:56:52 +0900

    버튼에 목적지 연결

--- src/pages/host/home/HostDashboardPage.jsx
+++ src/pages/host/home/HostDashboardPage.jsx
@@ -4,12 +4,16 @@ import HostHomeLayout from "../../../components/host/home/HostHomeLayout"
 import { useEffect, useState } from "react"
 import { getRecentRooms } from "../../../api/room/roomApi";
 import RecentRoomRow from "../../../components/host/home/RecentRoomRow";
+import { useNavigate } from "react-router-dom";
 
 
 
 const HostDashboardPage = () => {
 
     const [rooms, setRooms] = useState([]);
+
+
+    const nav = useNavigate();
 
 
     useEffect(() => {
@@ -32,11 +36,18 @@ const HostDashboardPage = () => {
 
                 <div className="host-dashboard-top">
                     <div className="host-dashboard-title-text">대시보드</div>
-                    <button className="host-dashboard-create-button"> + 말모임 만들기</button>
+                    <button
+                    className="host-dashboard-create-button"
+                    onClick={()=>nav('/createDetail')}
+                    > + 말모임 만들기</button>
                 </div>
                 <div className="host-dashboard-bottom">
                     <h2>최근 만든 말모임</h2>
-                    <div className="host-dashboard-all-button">전체보기 → </div>
+                    <div 
+                    className="host-dashboard-all-button"
+                    onClick={()=>nav('/myContent')}
+                    
+                    >전체보기 → </div>
                 </div>
                 <table className="host-dashboard-table">
                     <thead>
