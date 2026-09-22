3d57a3d5221ff22eb86b2e9e893898f01ba1adbf
domino3333 <busster@naver.com>
2026-09-22 21:56:11 +0900

    대시보드 버튼 css 수정

--- src/pages/host/home/HostHomePage.jsx
+++ src/pages/host/home/HostHomePage.jsx
@@ -12,7 +12,7 @@ const HostHomePage = () => {
 
                 <div className="host-dashboard-top">
                     <div className="host-dashboard-title-text">대시보드</div>
-                    <button className="host-dashboard-create-button">만들기</button>
+                    <button className="host-dashboard-create-button"> + 만들기</button>
                 </div>
                 <div className="host-dashboard-bottom">
                     <h2>최근 만든 말모임</h2>
