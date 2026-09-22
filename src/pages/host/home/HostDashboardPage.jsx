0f00a9ee7d198ff1c7af5a6886ec7ee457f08742
domino3333 <busster@naver.com>
2026-09-23 07:53:48 +0900

    버튼 및 html 구조 css 수정

--- src/pages/host/home/HostDashboardPage.jsx
+++ src/pages/host/home/HostDashboardPage.jsx
@@ -32,7 +32,7 @@ const HostDashboardPage = () => {
 
                 <div className="host-dashboard-top">
                     <div className="host-dashboard-title-text">대시보드</div>
-                    <button className="host-dashboard-create-button"> + 만들기</button>
+                    <button className="host-dashboard-create-button"> + 말모임 만들기</button>
                 </div>
                 <div className="host-dashboard-bottom">
                     <h2>최근 만든 말모임</h2>
