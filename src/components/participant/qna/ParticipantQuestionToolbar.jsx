425902331e27329539babfedc4cb8a75518c6a34
domino3333 <busster@naver.com>
2026-09-25 06:00:19 +0900

    참여자 페이지 css 수정

--- src/components/participant/qna/ParticipantQuestionToolbar.jsx
+++ src/components/participant/qna/ParticipantQuestionToolbar.jsx
@@ -5,7 +5,7 @@ const ParticipantQuestionToolbar = ({ onOpenQuestionModal }) => {
     return (<>
 
         <div className="ParticipantQuestionToolbar-parent-div">
-            <p>질문 리스트</p>
+            <h3>질문 리스트</h3>
             <button onClick={onOpenQuestionModal} className="ParticipantQuestionToolbar-register-button">등록하기</button>
         </div>
 
