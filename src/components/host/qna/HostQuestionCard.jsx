a80293c755f886d605c3b0ef43f40a449814049c
domino3333 <busster@naver.com>
2026-09-26 04:01:25 +0900

    호스트의 질문 카드 html 구조 변경

--- src/components/host/qna/HostQuestionCard.jsx
+++ src/components/host/qna/HostQuestionCard.jsx
@@ -4,7 +4,7 @@ import "../../../css/host/qna/HostQuestionCard.css"
 import { QUESTION_STATUS_LABELS } from "../../../constants/qna/statusLabels";
 
 // 질문 내용과 작성자·추천 정보를 표시하는 카드 컴포넌트
-const HostQuestionCard = ({ question, onQuestionSelect, showRank,showVoteCount }) => {
+const HostQuestionCard = ({ question, onQuestionSelect, showRank, showVoteCount }) => {
 
     return (<>
 
@@ -12,27 +12,32 @@ const HostQuestionCard = ({ question, onQuestionSelect, showRank,showVoteCount }
 
             {showRank &&
                 <div className="HostQuestionCard-rank-div">
-                {question.voteRank}
-            </div>
+                    {question.voteRank}
+                </div>
             }
 
             <div className="HostQuestionCard-main-div" onClick={() => onQuestionSelect(question)}>
                 <div className="HostQuestionCard-header-div">
                     <div className="HostQuestionCard-nickname-div">
-                        답변상태: {QUESTION_STATUS_LABELS[question.status] ?? question.status}
+                        닉네임:{question.nickname}
                     </div>
                     {showVoteCount &&
-                    <div className="HostQuestionCard-vote-div">
-                        추천수: {question.voteCount}
-                    </div>}
-                    
+                        <div className="HostQuestionCard-vote-div">
+                            추천수: {question.voteCount}
+                        </div>}
+
                 </div>
                 <div className="HostQuestionCard-body-div">
                     <div className="HostQuestionCard-body-question">
                         질문: {question.content}
                     </div>
-                    <div className="HostQuestionCard-body-time">
-                        {question.createdAt}
+                    <div>
+                        <div className="HostQuestionCard-body-time">
+                            {question.createdAt}
+                        </div>
+                        <div className="HostQuestionCard-status-div">
+                            답변상태: {QUESTION_STATUS_LABELS[question.status] ?? question.status}
+                        </div>
                     </div>
                 </div>
 
